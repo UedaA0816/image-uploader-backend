65099fdf2d2ae6e86cab6c2c4eb3bcc5a532febb
ueda <>
2021-08-26 11:19:28 +0900

    add file filter

--- src/tasks/upload_task.ts
+++ src/tasks/upload_task.ts
@@ -4,6 +4,15 @@ import { logger } from "./../logger";
 import { sleep } from "./../utils";
 
 export const upload_task = async (req:Request, res:Response) => {
+  logger.debug(req.file)
+
+  const isFileImage = req.file?.mimetype.includes("image")
+  if(!isFileImage) {
+    res.status(403).send({code:40301})
+    return
+  }
+
   await sleep(2000)
-  res.send({code:"ok",url:req.file?.path})
+  
+  res.send({code:20000,url:req.file?.path})
 }
